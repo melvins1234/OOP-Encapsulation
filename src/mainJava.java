99ae493ad247233d4ff321c5efc9f0cefdea87d1
Norimar Iguchi <gguchi193@gmail.com>
2021-03-30 21:41:31 +0800

    ADDED RANDOM BOY NAME GENERATOR

--- src/mainJava.java
+++ src/mainJava.java
@@ -1,5 +1,24 @@
+import java.util.Random;
+
 public class mainJava {
+
+    static Random rand = new Random();
+
     public static void main(String args[]){
-        
+
+
+        System.out.println(RandomBoyName());
+
+
+
     }
+
+    public static String RandomBoyName(){
+
+        String Name[]= {"Rey", "Aron", "Mike", "Steve", "John"};
+
+        return Name[rand.nextInt(5)];
+
+    }
+
 }
